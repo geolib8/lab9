efa3d700cf72fcc4005ac6b09faf388cdc5c48ed
geolib8 <42380925+geolib8@users.noreply.github.com>
2022-01-28 19:49:24 -0600

    lab9

---
+++ model/post.js
@@ -0,0 +1,12 @@
+const{Schema, model} = require("mongoose");
+
+const PostSchema = Schema({
+    title : String,
+    author : String,
+    post_date:{
+        type:Date,
+        default:Date.now
+    },
+    post_data:String
+})
+module.exports = model('tasks',PostSchema)
